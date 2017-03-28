0c00b97c955a90ef1e8c45fca4f84fea041ed6ea
林浩特 <lnhote@gmail.com>
2017-03-28 23:45:32 +0800

    for

--- src/hello/for.go
+++ src/hello/for.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
 	sum := 0
-	for i := 0; i < 10; i++ {
-		sum += i
+    for i := 0; i < 10; i++ {
+        sum += i
+    }
+    fmt.Println(sum)
+
+    sum1 := 1
+	for ; sum1 < 1000; {
+		sum1 += sum1
 	}
 	fmt.Println(sum)
 }
