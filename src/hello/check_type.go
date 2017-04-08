b570e7bfbc42e567d547ffe202c9f4b24c6cbe7f
林浩特 <lnhote@gmail.com>
2017-04-08 15:04:25 +0800

    ip example

--- src/hello/check_type.go
+++ src/hello/check_type.go
@@ -3,14 +3,26 @@ package main
 import "fmt"
 
 func check_type(i interface{}) {
-    switch v_type, ok := i.(type); ok {
+    switch v_type := i.(type) {
     case string:
-        fmt.Println("string")
+        fmt.Printf("string %v\n", v_type)
+    case int:
+        fmt.Printf("int %d\n", v_type)
+    case bool:
+        fmt.Printf("bool %v\n", v_type)
+    case float64:
+        fmt.Printf("float64 %v\n", v_type)
     default:
-
+        fmt.Println("wtf?\n")
 
     }
 }
 func main() {
-
+    var i interface{} = "Hunter"
+    val, ok := i.(string)
+    fmt.Printf("%s %v\n", val, ok)
+    val2, ok := i.(int)
+    fmt.Printf("%d %v\n", val2, ok)
+    check_type("Alice")
+    check_type(123)
 }
