b766a504177d909137502aab437d694d24e1379d
Hunter Lin <linhaote@didichuxing.com>
2017-04-08 15:01:45 +0800

    string

---
+++ src/hello/stringers.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+}
+
+func main() {
+	a := Person{"Arthur Dent", 42}
+	z := Person{"Zaphod Beeblebrox", 9001}
+	fmt.Println(a, z)
+
+	var ia interface{} = Person{"Hunter Lin", 42}
+	s, ok := ia.(Person)
+	fmt.Println(s, ok)
+}
